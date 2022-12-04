8b49ffeb2a99bc5ed6fd4905f2be83a1b4aad881
Zoltan Marton <martonzoltan4@gmail.com>
2022-12-04 10:06:53 +0200

    Solve day 1

---
+++ AdventOfCode2022_Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿IEnumerable<string> GetInput()
+{
+    var input = File.ReadAllLines(@"input.txt");
+    return input;
+}
+
+var listOfItems = GetInput();
+var singleElfCalories = 0;
+List<int> elfs = new();
+
+foreach (var entry in listOfItems)
+{
+    if (string.IsNullOrEmpty(entry))
+    {
+        elfs.Add(singleElfCalories);
+        singleElfCalories = 0;
+        continue;
+    }
+    
+    singleElfCalories += int.Parse(entry);
+}
+Console.WriteLine($"Max calories carried: {elfs.Max()}");
+Console.WriteLine($"Max calories by top 3: {elfs.OrderDescending().Take(3).Sum()}");
