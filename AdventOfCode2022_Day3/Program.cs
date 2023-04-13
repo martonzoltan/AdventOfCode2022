669b98ef8cb91960776a92f59867436f59593d3d
Zoltan Marton <martonzoltan4@gmail.com>
2023-04-13 11:29:35 +0300

    Small improvements

--- AdventOfCode2022_Day3/Program.cs
+++ AdventOfCode2022_Day3/Program.cs
@@ -12,18 +12,7 @@ foreach (var rucksack in sacks)
     var firstHalf = rucksack.Substring(0, rucksack.Length / 2);
     var secondHalf = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
     var common = firstHalf.Intersect(secondHalf);
-    foreach (var item in common)
-    {
-        if (item >= 97 && item <= 122)
-        {
-            total += item - 96;
-        }
-
-        if (item >= 65 && item <= 90)
-        {
-            total += item - 65 + 27;
-        }
-    }
+    total += common.Sum(GetPriority);
 }
 
 Console.WriteLine($"Total: {total}");
@@ -35,18 +24,22 @@ for (var elfNumber = 0; elfNumber < sacks.Length; elfNumber += 3)
     var elf2 = sacks[elfNumber + 1];
     var elf3 = sacks[elfNumber + 2];
     var common = elf1.Intersect(elf2).Intersect(elf3);
-    foreach (var item in common)
-    {
-        if (item >= 97 && item <= 122)
-        {
-            totalPart2 += item - 96;
-        }
-
-        if (item >= 65 && item <= 90)
-        {
-            totalPart2 += item - 65 + 27;
-        }
-    }
+    totalPart2 += GetPriority(common.FirstOrDefault());
 }
 
 Console.WriteLine($"Total part 2: {totalPart2}");
+
+int GetPriority(char item)
+{
+    if (item >= 97 && item <= 122)
+    {
+        return item - 96;
+    }
+
+    if (item >= 65 && item <= 90)
+    {
+        return item - 65 + 27;
+    }
+
+    return 0;
+}
